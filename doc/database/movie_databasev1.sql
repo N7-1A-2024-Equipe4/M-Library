977d49d6e0eb9a9f84c4e78dffb51c16e6cc316c
XU Anton <anxton4@gmail.com>
2024-05-28 16:55:54 +0200

    [#60] Change names to singular and add movie director foreign key

--- doc/database/movie_databasev1.sql
+++ doc/database/movie_databasev1.sql
@@ -1,54 +1,42 @@
-DROP DATABASE IF EXISTS movie_database;
+DROP
+DATABASE IF EXISTS mlibrary;
 
-CREATE DATABASE movie_database;
+CREATE
+DATABASE mlibrary;
 
-USE movie_database;
+USE
+mlibrary;
 
-DROP TABLE IF EXISTS movies;
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS lists;
-DROP TABLE IF EXISTS reviews;
-DROP TABLE IF EXISTS people;
-DROP TABLE IF EXISTS actors;
-DROP TABLE IF EXISTS screenwriters;
-DROP TABLE IF EXISTS directors;
-DROP TABLE IF EXISTS castings;
-DROP TABLE IF EXISTS saved_lists;
+DROP TABLE IF EXISTS movie;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS list;
+DROP TABLE IF EXISTS review;
+DROP TABLE IF EXISTS person;
+DROP TABLE IF EXISTS actor;
+DROP TABLE IF EXISTS screenwriter;
+DROP TABLE IF EXISTS director;
+DROP TABLE IF EXISTS casting;
+DROP TABLE IF EXISTS movie_in_list;
 
-CREATE TABLE movies (
-    movie_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    genre ENUM('Action', 'Comedy', 'Drama', 'Horror', 'Romance', 'Sci-Fi', 'Documentary'),
-    duration INT,
-    image LONGBLOB, -- // Since this database will be used for a small application, we choose to store the image directly in the database
-    synopsis TEXT(1000) NOT NULL
-);
-
-CREATE TABLE users (
+CREATE TABLE user
+(
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE lists (
+CREATE TABLE list
+(
     list_id INT AUTO_INCREMENT PRIMARY KEY,
     list_name VARCHAR(255) NOT NULL,
     icon LONGBLOB,
     creation_date DATE NOT NULL,
     user_id INT,
     description TEXT(1000),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    FOREIGN KEY (user_id) REFERENCES user (user_id)
 );
 
-CREATE TABLE reviews (
-    review_id INT AUTO_INCREMENT PRIMARY KEY,
-    review TEXT(1000) NOT NULL,
-    user_id INT,
-    movie_id INT,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (movie_id) REFERENCES movies(movie_id)
-);
-
-CREATE TABLE people (
+CREATE TABLE person
+(
     person_id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
@@ -56,35 +44,62 @@ CREATE TABLE people (
     date_of_death DATE
 );
 
-CREATE TABLE actors (
+CREATE TABLE actor
+(
     actor_id INT AUTO_INCREMENT PRIMARY KEY,
     person_id INT,
-    FOREIGN KEY (person_id) REFERENCES people(person_id)
+    FOREIGN KEY (person_id) REFERENCES person (person_id)
 );
 
-CREATE TABLE screenwriters (
+CREATE TABLE screenwriter
+(
     screenwriter_id INT AUTO_INCREMENT PRIMARY KEY,
     person_id INT,
-    FOREIGN KEY (person_id) REFERENCES people(person_id)
+    FOREIGN KEY (person_id) REFERENCES person (person_id)
 );
 
-CREATE TABLE directors (
+CREATE TABLE director
+(
     director_id INT AUTO_INCREMENT PRIMARY KEY,
     person_id INT,
-    FOREIGN KEY (person_id) REFERENCES people(person_id)
+    FOREIGN KEY (person_id) REFERENCES person (person_id)
 );
 
-CREATE TABLE castings (
+CREATE TABLE movie
+(
+    movie_id    INT AUTO_INCREMENT PRIMARY KEY,
+    title       VARCHAR(255) NOT NULL,
+    genre       ENUM('Action', 'Comedy', 'Drama', 'Horror', 'Romance', 'Sci-Fi', 'Documentary'),
+    duration    INT,
+    image       LONGBLOB, -- Since this database will be used for a small application, we choose to store the image directly in the database
+    synopsis    TEXT(1000) NOT NULL,
+    director_id INT,
+    FOREIGN KEY (director_id) REFERENCES director (director_id)
+);
+
+CREATE TABLE review
+(
+    review_id INT AUTO_INCREMENT PRIMARY KEY,
+    review    TEXT(1000) NOT NULL,
+    user_id   INT,
+    movie_id  INT,
+    FOREIGN KEY (user_id) REFERENCES user (user_id),
+    FOREIGN KEY (movie_id) REFERENCES movie (movie_id)
+);
+
+CREATE TABLE casting
+(
     casting_id INT AUTO_INCREMENT PRIMARY KEY,
     actor_id INT,
     movie_id INT,
-    FOREIGN KEY (actor_id) REFERENCES actors(actor_id),
-    FOREIGN KEY (movie_id) REFERENCES movies(movie_id)
+    FOREIGN KEY (actor_id) REFERENCES actor (actor_id),
+    FOREIGN KEY (movie_id) REFERENCES movie (movie_id)
 );
 
-CREATE TABLE saved_lists (
+CREATE TABLE movie_in_list
+(
     movie_id INT,
     list_id INT,
-    FOREIGN KEY (movie_id) REFERENCES movies(movie_id),
-    FOREIGN KEY (list_id) REFERENCES lists(list_id)
+    FOREIGN KEY (movie_id) REFERENCES movie (movie_id),
+    FOREIGN KEY (list_id) REFERENCES list (list_id)
 );
