c0879044f5e114a8387add362937de7734d3879d
XU Anton <anxton4@gmail.com>
2024-05-31 12:48:00 +0200

    [#25] add username and password change actions

--- src/main/java/controller/ProfileController.java
+++ src/main/java/controller/ProfileController.java
@@ -5,24 +5,63 @@ import model.User;
 import session.Session;
 import view.ProfileView;
 
+import java.sql.SQLException;
+
 public class ProfileController {
     private final ProfileView view;
 
+    private final UserDAO userDao;
+
     public ProfileController(ProfileView view) {
         this.view = view;
+        this.userDao = new UserDAO();
     }
 
     public void usernameChangeAction(String username) {
-        UserDAO.updateUserByUsername(Session.getUsername(), new User(username, null));
-        if (Session.login(username)) {
+        // copy session user
+        User newUser = new User(Session.getUser());
+        // set new username
+        newUser.setUsername(username);
+        // update in database
+        try {
+            userDao.update(newUser.getId(), newUser);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            view.usernameChangeFailed();
+            return;
+        }
+        // log back in
+        Session.logout();
+        if (Session.login(newUser.getUsername(), newUser.getPassword())) {
             MainController.getInstance().updateNavbar();
             view.refresh();
             view.usernameChangeSuccess(username);
+        } else {
+            view.weirdFail();
         }
     }
 
     public void passwordAction(String password) {
-        UserDAO.updateUserByUsername(Session.getUsername(), new User(null, password));
-        view.refresh();
+        // copy session user
+        User newUser = new User(Session.getUser());
+        // set new password
+        newUser.setPassword(password);
+        // update in database
+        try {
+            userDao.update(newUser.getId(), newUser);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            view.passwordChangeFailed();
+            return;
+        }
+        // log back in
+        Session.logout();
+        if (Session.login(newUser.getUsername(), newUser.getPassword())) {
+            MainController.getInstance().updateNavbar();
+            view.refresh();
+            view.passwordChangeSuccess();
+        } else {
+            view.weirdFail();
+        }
     }
 }
