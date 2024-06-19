8c804b06b64172a184401f637190d82e7b935354
Paul-Arago <paularago@outlook.fr>
2024-06-19 17:06:22 +0200

    Remove all in refresh libraries

--- src/main/java/view/LibraryView.java
+++ src/main/java/view/LibraryView.java
@@ -2,46 +2,34 @@ package view;
 
 import model.ElementOfLibrary;
 import model.Library;
-import model.Movie;
 import service.LibraryService;
 import utils.image.ImageUtil;
 
+import javax.sql.rowset.BaseRowSet;
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 import java.sql.SQLException;
 import java.util.List;
+import controller.LibraryController;
 
 public class LibraryView implements View {
     private JPanel mainPanel;
-    private JPanel informationsPanel;
     private JPanel movieListPanel;
-    private JPanel presentationPanel;
-    private JPanel descriptionPanel;
-    private JPanel movieIconPanel;
-    private JLabel titleLabel;
-    private JLabel authorLabel;
-    private JLabel creationDateLabel;
-    private JLabel descriptionTitleLabel;
-    private JLabel descriptionLabel;
-    private JLabel movieIconLabel;
-    private JPanel panelInfoLibrary;
-    private JScrollPane mainScrollPain;
-    private JPanel mainPanelForScroll;
-    private JButton deleteLibraryButton;
+    private final LibraryController controller;
 
     private final int WIDTH = 200;
     private final int HEIGHT = 200;
 
-    private LibraryService libraryService;
+    private final LibraryService libraryService;
 
     public LibraryView() {
         this.libraryService = new LibraryService();
+        this.controller = new LibraryController(this);
         setupUI();
     }
 
     private void setupUI() {
-        presentationPanel.setPreferredSize(new Dimension(mainPanel.getWidth(), HEIGHT));
+        this.mainPanel = new JPanel(new BorderLayout());
     }
 
     @Override
@@ -53,10 +41,54 @@ public class LibraryView implements View {
     public void refresh(Integer modelId) {
         try {
             mainPanel.removeAll();
+
+            JPanel informationsPanel = new JPanel();
+            informationsPanel.setLayout(new BoxLayout(informationsPanel, BoxLayout.Y_AXIS));
+
+            JPanel presentationPanel = new JPanel();
+            presentationPanel.setLayout(new BoxLayout(presentationPanel, BoxLayout.Y_AXIS));
+
+            JPanel movieIconPanel = new JPanel(new BorderLayout());
+            JLabel movieIconLabel = new JLabel();
             Library library = this.libraryService.getById(modelId);
             if (library.getIcon() != null) {
                 movieIconLabel.setIcon(new ImageIcon(ImageUtil.getScaledImage(library.getIcon().getImage(), WIDTH, HEIGHT)));
             }
+            movieIconPanel.add(movieIconLabel, BorderLayout.CENTER);
+
+            JPanel panelInfoLibrary = new JPanel();
+            JPanel borderPanelForInfoLibrary = new JPanel(new BorderLayout());
+            panelInfoLibrary.setLayout(new BoxLayout(panelInfoLibrary, BoxLayout.Y_AXIS));
+            JLabel titleLabel = new JLabel();
+            JLabel creationDateLabel = new JLabel();
+            JLabel authorLabel = new JLabel();
+            panelInfoLibrary.add(titleLabel);
+            panelInfoLibrary.add(creationDateLabel);
+            panelInfoLibrary.add(authorLabel);
+            borderPanelForInfoLibrary.add(panelInfoLibrary, BorderLayout.WEST);
+
+            presentationPanel.add(movieIconPanel);
+            presentationPanel.add(borderPanelForInfoLibrary);
+
+            JPanel borderPanelForDescription = new JPanel(new BorderLayout());
+            JPanel descriptionPanel = new JPanel();
+            descriptionPanel.setLayout(new BoxLayout(descriptionPanel, BoxLayout.Y_AXIS));
+
+            JLabel descriptionTitleLabel = new JLabel("Description");
+            JLabel descriptionLabel = new JLabel();
+            JButton buttonDelete = new JButton(("Delete library"));
+            buttonDelete.addActionListener(e -> controller.deleteLibraryAction(modelId));
+            JLabel moviesLabel = new JLabel("Movies");
+            moviesLabel.setFont(new Font("Arial", Font.BOLD, 20));
+
+            descriptionPanel.add(descriptionTitleLabel);
+            descriptionPanel.add(descriptionLabel);
+            descriptionPanel.add(buttonDelete);
+            descriptionPanel.add(moviesLabel);
+            borderPanelForDescription.add(descriptionPanel);
+
+            informationsPanel.add(presentationPanel);
+            informationsPanel.add(borderPanelForDescription);
 
             titleLabel.setText(library.getName());
             titleLabel.setFont(new Font("Arial", Font.BOLD, 20));
@@ -65,16 +97,15 @@ public class LibraryView implements View {
             descriptionLabel.setText(library.getDescription());
             descriptionTitleLabel.setFont(new Font("Arial", Font.BOLD, 20));
 
-            mainPanel.setLayout(new GridLayout(0,1));
             movieListPanel = new JPanel(new GridLayout(0,1, 0,20));
             displayMovies(library);
-            mainPanelForScroll = new JPanel();
+            JPanel mainPanelForScroll = new JPanel();
             mainPanelForScroll.setLayout(new BoxLayout(mainPanelForScroll, BoxLayout.Y_AXIS));
             mainPanelForScroll.add(informationsPanel);
             mainPanelForScroll.add(movieListPanel);
-            mainScrollPain = new JScrollPane(mainPanelForScroll, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+            JScrollPane mainScrollPain = new JScrollPane(mainPanelForScroll, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
             mainScrollPain.getVerticalScrollBar().setUnitIncrement(16);
-            mainPanel.add(mainScrollPain);
+            mainPanel.add(mainScrollPain, BorderLayout.CENTER);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -90,11 +121,11 @@ public class LibraryView implements View {
 
                 JPanel titlePanel = new JPanel(new BorderLayout());
                 JLabel titleLabel = new JLabel(element.getMovie().getTitle());
+                titleLabel.setFont(new Font("Arial", Font.BOLD, 14));
                 titlePanel.add(titleLabel, BorderLayout.WEST);
 
                 JPanel movieInformationsPanel = new JPanel();
-                movieInformationsPanel.setLayout(/*new GridLayout(1, 2, 0, 0)*/
-                        new BoxLayout(movieInformationsPanel, BoxLayout.Y_AXIS));
+                movieInformationsPanel.setLayout(new BoxLayout(movieInformationsPanel, BoxLayout.Y_AXIS));
 
                 JLabel moviePosterLabel = new JLabel();
                 if (element.getMovie().getPoster() != null) {
@@ -107,6 +138,8 @@ public class LibraryView implements View {
                 descriptionAndRatingPanel.setLayout(new GridLayout(2, 1, 0, 0));
                 JLabel ratingLabel = new JLabel(Float.toString(element.getMovie().getRating()));
                 JLabel noteLabel = new JLabel(element.getNote());
+                ratingLabel.setFont(new Font("Arial", Font.BOLD,14));
+                noteLabel.setFont(new Font("Arial", Font.PLAIN,16));
                 descriptionAndRatingPanel.add(noteLabel);
                 descriptionAndRatingPanel.add(ratingLabel);
 
